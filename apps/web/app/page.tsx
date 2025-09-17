f133d3c9265844b81840fbc65144b11bd9b7db3b
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2025-09-17 20:38:34 +0530

    next.js build error

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import {client} from '@repo/prisma-db/client'
-
-
+import { client } from "@repo/prisma-db/client";
 
 export default async function Home() {
-
-  const user = await client.user.findFirst()
+  const user = await client.user.findFirst();
 
   return (
     <div className={styles.page}>
